58970e04c2051376c46adbbd02853f7ce404ffd2
yuki.arashi <yuki.arashi@broadleaf.co.jp>
2019-02-07 16:11:05 +0900

    チャットテスト

--- src/views/chatroom-page/chatroom-page.vue
+++ src/views/chatroom-page/chatroom-page.vue
@@ -1,19 +1,29 @@
 <template>
   <v-container class="chat-container">
-    <v-layout class="chat-message-container"
-    row
-    align-start
-    >
+    <!-- chat!! -->
+    <v-layout class="chat-message-container">
       <div class="balloon-set-box left">
         <div class="icon-box">
-          <img src="image.jpg" width="40" height="40" class="icon">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1141818/profile/profile-80.jpg" width="40" height="40" class="icon">
         </div>
         <div class="balloon">左からのコメント</div>
       </div>
+      <div class="balloon-set-box right">
+      <div class="icon-box">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1141818/profile/profile-80.jpg" width="40" height="40" class="icon">
+      </div>
+      <div class="balloon">右からの長ーーーーーーーーーーーーーーーーーーーーーーーーーーーーいコメント</div>
+      </div>
+      <div class="balloon-set-box right">
+      <div class="icon-box">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1141818/profile/profile-80.jpg" width="40" height="40" class="icon">
+      </div>
+      <div class="balloon">わいわい</div>
+      </div>
     </v-layout>
+     <!-- chat end. -->
     <v-layout style="margin-top: 25px;"
     row
-    fill-height
     align-end>
       <chat-form></chat-form>
     </v-layout>
